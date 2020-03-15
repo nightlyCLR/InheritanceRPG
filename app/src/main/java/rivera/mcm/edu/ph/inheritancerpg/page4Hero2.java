e9ce739fbef9aff34bae7bc7f64e4e855a754e7d
nightlyCLR <59899589+nightlyCLR@users.noreply.github.com>
2020-03-15 21:17:35 +0800

    still needs work

--- app/src/main/java/rivera/mcm/edu/ph/inheritancerpg/page4Hero2.java
+++ app/src/main/java/rivera/mcm/edu/ph/inheritancerpg/page4Hero2.java
@@ -46,7 +46,7 @@ public class page4Hero2 extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        if (value.equals("Warrior")){
+                        if ("Warrior".equals(value)){
                             Hero kay = new Hero (6,1,250,100,20,10,30,10,"Warrior",
                                     40,18,15,5,5,5);
 
@@ -60,7 +60,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                         }
-                        else if (value.equals("Tank")){
+                        else if ("Tank".equals(value)){
                             Hero kay = new Hero (7,1,400,100,40,15,100,100,"Tank",
                                     40,18,15,5,5,5);
 
@@ -74,7 +74,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                     }
-                        else if (value.equals("Samurai")){
+                        else if ("Samurai".equals(value)){
                             Hero kay = new Hero (8,1,350,120,55,20,90,80,"Samurai",
                                     30,30,10,5,5,5);
 
@@ -88,7 +88,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                         }
-                        else if (value.equals("Mage")){
+                        else if ("Mage".equals(value)){
                             Hero kay = new Hero (9,1,150,250,23,30,20,20,"Mage",
                                     30,30,10,5,5,5);
 
@@ -102,7 +102,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                         }
-                        else if (value.equals("Summoner")){
+                        else if ("Summoner".equals(value)){
                             Hero kay = new Hero (10,1,350,300,15,30,20,30,"Summoner",
                                     30,30,10,5,5,5);
 
@@ -116,7 +116,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                         }
-                        else if (value.equals("Alchemist")){
+                        else if ("Alchemist".equals(value)){
                             Hero kay = new Hero (11,1,200,250,10,45,35,35,"Alchemist",
                                     30,30,10,5,5,5);
 
@@ -130,7 +130,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                         }
-                        else if (value.equals("Rogue")){
+                        else if ("Rogue".equals(value)){
                             Hero kay = new Hero (12,1,200,145,35,20,25,25,"Rogue",
                                     30,30,10,5,5,5);
 
@@ -144,7 +144,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                         }
-                        else if (value.equals("Assassin")){
+                        else if ("Assassin".equals(value)){
                             Hero kay = new Hero (13,1,200,200,40,35,20,20,"Assasin",
                                     30,30,10,5,5,5);
 
@@ -158,7 +158,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                         }
-                        else if (value.equals("Hunter")){
+                        else if ("Hunter".equals(value)){
                             Hero kay = new Hero (14,1,150,250,50,10,20,20,"Hunter",
                                     30,30,10,5,5,5);
 
@@ -172,7 +172,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                         }
-                        else if (value.equals("Cleric")){
+                        else if ("Cleric".equals(value)){
                             Hero kay = new Hero (15,1,250,100,25,30,45,45,"Cleric",
                                     30,30,10,5,5,5);
 
@@ -186,7 +186,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                         }
-                        else if (value.equals("Paladin")){
+                        else if ("Paladin".equals(value)){
                             Hero kay = new Hero (16,1,300,100,45,25,50,50,"Paladin",
                                     30,30,10,5,5,5);
 
@@ -200,7 +200,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                         }
-                        else if (value.equals("White Mage")){
+                        else if ("White Mage".equals(value)){
                             Hero kay = new Hero (17,1,200,300,15,40,40,40,"White Mage",
                                     30,30,10,5,5,5);
 
@@ -214,7 +214,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                         }
-                        else if (value.equals("Archer")){
+                        else if ("Archer".equals(value)){
                             Hero kay = new Hero (18,1,150,135,50,10,25,20,"Archer",
                                     30,30,10,5,5,5);
 
@@ -228,7 +228,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                         }
-                        else if (value.equals("Bard")){
+                        else if ("Bard".equals(value)){
                             Hero kay = new Hero (19,1,160,150,55,5,20,20,"Bardr",
                                     30,30,10,5,5,5);
 
@@ -242,7 +242,7 @@ public class page4Hero2 extends AppCompatActivity {
                             h.setText(String.valueOf(kay.getBaseAGI()));
                             i.setText(String.valueOf(kay.getBaseINT()));
                         }
-                        else if (value.equals("Dancer")){
+                        else if ("Dancer".equals(value)){
                             Hero kay = new Hero (20,1,150,100,10,30,25,25,"Dancer",
                                     30,30,10,5,5,5);
 
